10a2d27758c061104ddcd6c98f4d543bc0f082e3
st-ea <105039095+st-ea@users.noreply.github.com>
2022-05-15 23:54:45 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -14,7 +14,7 @@ class TestProductPage:
 
         # Установлено время принудительной задержки браузера
         # после открытия страницы, для визуальной проверки языка открытой страницы
-        time.sleep(10)
+        time.sleep(30)
 
         # Проверяем наличие кнопки добавления товара в корзину
         assert browser.find_element_by_css_selector("button.btn-add-to-basket")
